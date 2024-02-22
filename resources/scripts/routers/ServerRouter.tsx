a159bb37166605d39e20e6fe3b316abafc6b1078
fero <hi@evan.graphics>
2024-02-22 15:50:29 -0700

    feat: pyro logo should link back to home page

--- resources/scripts/routers/ServerRouter.tsx
+++ resources/scripts/routers/ServerRouter.tsx
@@ -1,6 +1,6 @@
 import TransferListener from '@/components/server/TransferListener';
 import React, { useEffect, useState } from 'react';
-import { NavLink, Route, Switch, useRouteMatch } from 'react-router-dom';
+import { Link, NavLink, Route, Switch, useRouteMatch } from 'react-router-dom';
 import NavigationBar from '@/components/NavigationBar';
 import TransitionRouter from '@/TransitionRouter';
 import WebsocketHandler from '@/components/server/WebsocketHandler';
@@ -75,52 +75,52 @@ export default () => {
                 )
             ) : (
                 <>
-                    <CSSTransition timeout={150} classNames={'fade'} appear in>
-                        <SubNavigation>
-                            <div className='flex flex-row items-center justify-between h-8'>
-                                <div className='flex shrink-0 h-full w-fit'>
-                                    <Logo />
-                                </div>
-                                <div className='flex shrink-0 h-6 w-6 fill-white'>
-                                    <svg
-                                        xmlns='http://www.w3.org/2000/svg'
-                                        width='32'
-                                        height='32'
-                                        fill='currentColor'
-                                        viewBox='0 0 256 256'
-                                        className='flex shrink-0 h-full w-full'
-                                    >
-                                        {/* @ts-ignore */}
-                                        <path d='M138,128a10,10,0,1,1-10-10A10,10,0,0,1,138,128ZM60,118a10,10,0,1,0,10,10A10,10,0,0,0,60,118Zm136,0a10,10,0,1,0,10,10A10,10,0,0,0,196,118Z'></path>
-                                    </svg>
-                                </div>
+                    {/* <CSSTransition timeout={150} classNames={'fade'} appear in> */}
+                    <SubNavigation>
+                        <div className='flex flex-row items-center justify-between h-8'>
+                            <Link to={'/'} className='flex shrink-0 h-full w-fit'>
+                                <Logo />
+                            </Link>
+                            <div className='flex shrink-0 h-6 w-6 fill-white'>
+                                <svg
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    width='32'
+                                    height='32'
+                                    fill='currentColor'
+                                    viewBox='0 0 256 256'
+                                    className='flex shrink-0 h-full w-full'
+                                >
+                                    {/* @ts-ignore */}
+                                    <path d='M138,128a10,10,0,1,1-10-10A10,10,0,0,1,138,128ZM60,118a10,10,0,1,0,10,10A10,10,0,0,0,60,118Zm136,0a10,10,0,1,0,10,10A10,10,0,0,0,196,118Z'></path>
+                                </svg>
                             </div>
-                            <div className='mt-6 mb-4 bg-[#ffffff33] h-[1px] w-8'></div>
-                            <div className='pyro-subnav-routes-wrapper'>
-                                {routes.server
-                                    .filter((route) => !!route.name)
-                                    .map((route) =>
-                                        route.permission ? (
-                                            <Can key={route.path} action={route.permission} matchAny>
-                                                <NavLink to={to(route.path, true)} exact={route.exact}>
-                                                    {route.name}
-                                                </NavLink>
-                                            </Can>
-                                        ) : (
-                                            <NavLink key={route.path} to={to(route.path, true)} exact={route.exact}>
+                        </div>
+                        <div className='mt-6 mb-4 bg-[#ffffff33] h-[1px] w-8'></div>
+                        <div className='pyro-subnav-routes-wrapper'>
+                            {routes.server
+                                .filter((route) => !!route.name)
+                                .map((route) =>
+                                    route.permission ? (
+                                        <Can key={route.path} action={route.permission} matchAny>
+                                            <NavLink to={to(route.path, true)} exact={route.exact}>
                                                 {route.name}
                                             </NavLink>
-                                        )
-                                    )}
-                                {rootAdmin && (
-                                    // eslint-disable-next-line react/jsx-no-target-blank
-                                    <a href={`/admin/servers/view/${serverId}`} target={'_blank'}>
-                                        <FontAwesomeIcon icon={faExternalLinkAlt} />
-                                    </a>
+                                        </Can>
+                                    ) : (
+                                        <NavLink key={route.path} to={to(route.path, true)} exact={route.exact}>
+                                            {route.name}
+                                        </NavLink>
+                                    )
                                 )}
-                            </div>
-                        </SubNavigation>
-                    </CSSTransition>
+                            {rootAdmin && (
+                                // eslint-disable-next-line react/jsx-no-target-blank
+                                <a href={`/admin/servers/view/${serverId}`} target={'_blank'}>
+                                    <FontAwesomeIcon icon={faExternalLinkAlt} />
+                                </a>
+                            )}
+                        </div>
+                    </SubNavigation>
+                    {/* </CSSTransition> */}
                     <InstallListener />
                     <TransferListener />
                     <WebsocketHandler />
