5ac9135a5363b8f072ecae3eb09225d857d8f66d
fero <hi@evan.graphics>
2024-02-24 15:37:02 -0700

    feat: update copy on click modal

--- resources/scripts/components/elements/CopyOnClick.tsx
+++ resources/scripts/components/elements/CopyOnClick.tsx
@@ -32,6 +32,7 @@ const CopyOnClick = ({ text, showInNotification = true, children }: CopyOnClickP
     const child = !text
         ? React.Children.only(children)
         : React.cloneElement(React.Children.only(children), {
+              // @ts-ignore
               className: classNames(children.props.className || '', 'cursor-pointer'),
               onClick: (e: React.MouseEvent<HTMLElement>) => {
                   copy(String(text));
@@ -46,10 +47,20 @@ const CopyOnClick = ({ text, showInNotification = true, children }: CopyOnClickP
         <>
             {copied && (
                 <Portal>
-                    <Fade in appear timeout={250} key={copied ? 'visible' : 'invisible'}>
-                        <div className={'fixed z-50 bottom-0 right-0 m-4'}>
-                            <div className={'rounded-md py-3 px-4 text-zinc-200 bg-zinc-600/95 shadow'}>
-                                <p>
+                    <Fade in unmountOnExit timeout={250} key={copied ? 'visible' : 'invisible'}>
+                        <div className={'fixed flex items-center justify-center top-0 w-full p-8 z-[9999]'}>
+                            <div
+                                className={
+                                    'w-fit rounded-2xl py-3 px-4 text-zinc-200 skeleton-anim-2 duration-150 border border-[#ffffff07] shadow-lg select-none pointer-events-none backdrop-blur-xl'
+                                }
+                                style={{
+                                    background:
+                                        'radial-gradient(124.75% 124.75% at 50.01% -10.55%, rgba(46, 46, 46, 0.3) 0%, rgb(26, 26, 26, 0.2) 100%)',
+                                    animationTimingFunction:
+                                        'linear(0,0.01,0.04 1.6%,0.161 3.3%,0.816 9.4%,1.046,1.189 14.4%,1.231,1.254 17%,1.259,1.257 18.6%,1.236,1.194 22.3%,1.057 27%,0.999 29.4%,0.955 32.1%,0.942,0.935 34.9%,0.933,0.939 38.4%,1 47.3%,1.011,1.017 52.6%,1.016 56.4%,1 65.2%,0.996 70.2%,1.001 87.2%,1)',
+                                }}
+                            >
+                                <p className='text-sm font-bold'>
                                     {showInNotification
                                         ? `Copied "${String(text)}" to clipboard.`
                                         : 'Copied text to clipboard.'}
