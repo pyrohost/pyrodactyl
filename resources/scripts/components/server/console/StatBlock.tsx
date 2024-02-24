e23fb1acce46b60ada8bf51f0418ecdce74fdc6c
fero <hi@evan.graphics>
2024-02-24 11:47:05 -0700

    build: remove 'use-fit-text'

--- resources/scripts/components/server/console/StatBlock.tsx
+++ resources/scripts/components/server/console/StatBlock.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import Icon from '@/components/elements/Icon';
 import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
 import classNames from 'classnames';
 import styles from './style.module.css';
-import useFitText from 'use-fit-text';
 import CopyOnClick from '@/components/elements/CopyOnClick';
 
 interface StatBlockProps {
