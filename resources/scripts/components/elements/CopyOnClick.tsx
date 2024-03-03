d8625546d0f9baa663ded5d40a88583b123260ef
fero <hi@evan.graphics>
2024-03-03 13:54:19 -0700

    feat: use sonner for copyonclick

--- resources/scripts/components/elements/CopyOnClick.tsx
+++ resources/scripts/components/elements/CopyOnClick.tsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from 'react';
 import * as React from 'react';
-import Fade from '@/components/elements/Fade';
-import Portal from '@/components/elements/Portal';
 import copy from 'copy-to-clipboard';
 import clsx from 'clsx';
+import { toast } from 'sonner';
 
 interface CopyOnClickProps {
     text: string | number | null | undefined;
@@ -11,11 +10,12 @@ interface CopyOnClickProps {
     children: React.ReactNode;
 }
 
-const CopyOnClick = ({ text, showInNotification = true, children }: CopyOnClickProps) => {
+const CopyOnClick = ({ text, children }: CopyOnClickProps) => {
     const [copied, setCopied] = useState(false);
 
     useEffect(() => {
         if (!copied) return;
+        toast(`Copied ${text} to clipboard`);
 
         const timeout = setTimeout(() => {
             setCopied(false);
@@ -44,36 +44,7 @@ const CopyOnClick = ({ text, showInNotification = true, children }: CopyOnClickP
               },
           });
 
-    return (
-        <>
-            {copied && (
-                <Portal>
-                    <Fade in unmountOnExit timeout={250} key={copied ? 'visible' : 'invisible'}>
-                        <div className={'fixed flex items-center justify-center top-0 w-full p-8 z-[9999]'}>
-                            <div
-                                className={
-                                    'w-fit rounded-2xl py-3 px-4 text-zinc-200 skeleton-anim-2 duration-150 border border-[#ffffff07] shadow-lg select-none pointer-events-none backdrop-blur-xl'
-                                }
-                                style={{
-                                    background:
-                                        'radial-gradient(124.75% 124.75% at 50.01% -10.55%, rgba(46, 46, 46, 0.3) 0%, rgb(26, 26, 26, 0.2) 100%)',
-                                    animationTimingFunction:
-                                        'linear(0,0.01,0.04 1.6%,0.161 3.3%,0.816 9.4%,1.046,1.189 14.4%,1.231,1.254 17%,1.259,1.257 18.6%,1.236,1.194 22.3%,1.057 27%,0.999 29.4%,0.955 32.1%,0.942,0.935 34.9%,0.933,0.939 38.4%,1 47.3%,1.011,1.017 52.6%,1.016 56.4%,1 65.2%,0.996 70.2%,1.001 87.2%,1)',
-                                }}
-                            >
-                                <p className='text-sm font-bold'>
-                                    {showInNotification
-                                        ? `Copied "${String(text)}" to clipboard.`
-                                        : 'Copied text to clipboard.'}
-                                </p>
-                            </div>
-                        </div>
-                    </Fade>
-                </Portal>
-            )}
-            {child}
-        </>
-    );
+    return <>{child}</>;
 };
 
 export default CopyOnClick;
