772107c2596f6ae38153cc9590d22035e1e4ea72
fero <hi@evan.graphics>
2024-03-02 19:43:15 -0700

    build: update react index

--- resources/scripts/index.tsx
+++ resources/scripts/index.tsx
@@ -1,5 +1,4 @@
-import * as React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import App from '@/components/App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+render(<App />, document.getElementById('app'));
