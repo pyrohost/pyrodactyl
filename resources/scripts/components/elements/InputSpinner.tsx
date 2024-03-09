10263dde16e479e28032fa36e8529d4687104164
fero <hi@evan.graphics>
2024-03-09 13:53:56 -0700

    chore: fix inputSpinner component

--- resources/scripts/components/elements/InputSpinner.tsx
+++ resources/scripts/components/elements/InputSpinner.tsx
@@ -1,11 +1,12 @@
 import Spinner from '@/components/elements/Spinner';
 import styled, { css } from 'styled-components';
 import Select from '@/components/elements/Select';
+import FadeTransition from './transitions/FadeTransition';
 
-const Container = styled.div<{ $visible?: boolean }>`
+const Container = styled.div<{ visible?: boolean }>`
     position: relative
         ${(props) =>
-            props.$visible &&
+            props.visible &&
             css`
                 & ${Select} {
                     background-image: none;
@@ -14,10 +15,20 @@ const Container = styled.div<{ $visible?: boolean }>`
 `;
 
 const InputSpinner = ({ visible, children }: { visible: boolean; children: React.ReactNode }) => (
-    <Container $visible={visible}>
-        <div className={`absolute right-0 h-full flex items-center justify-end pr-3`}>
-            <Spinner size={'small'} />
-        </div>
+    <Container visible={visible}>
+        <FadeTransition
+            css={`
+                position: relative;
+            `}
+            show={visible}
+            duration='duration-150'
+            appear
+            unmount
+        >
+            <div className={`absolute right-0 h-full flex items-center justify-end pr-3`}>
+                <Spinner size='small' />
+            </div>
+        </FadeTransition>
         {children}
     </Container>
 );
