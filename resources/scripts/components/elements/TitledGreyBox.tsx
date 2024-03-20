0a7fed49112567ed340c583165a176849a82bf12
fero <hi@evan.graphics>
2024-03-19 17:14:14 -0700

    chore: allow titledgreyboxes to have no title

--- resources/scripts/components/elements/TitledGreyBox.tsx
+++ resources/scripts/components/elements/TitledGreyBox.tsx
@@ -2,20 +2,22 @@ import { memo } from 'react';
 import isEqual from 'react-fast-compare';
 
 interface Props {
-    title: string | React.ReactNode;
+    title?: string | React.ReactNode;
     className?: string;
     children: React.ReactNode;
 }
 
 const TitledGreyBox = ({ title, children }: Props) => (
     <div className={`relative rounded-xl overflow-hidden shadow-md border-[1px] border-[#ffffff07] bg-[#ffffff08] p-8`}>
-        <div>
-            {typeof title === 'string' ? (
-                <p className={`text-xl font-extrabold tracking-tight mb-4`}>{title}</p>
-            ) : (
-                title
-            )}
-        </div>
+        {title && (
+            <div>
+                {typeof title === 'string' ? (
+                    <p className={`text-xl font-extrabold tracking-tight mb-4`}>{title}</p>
+                ) : (
+                    title
+                )}
+            </div>
+        )}
         <div className='w-full h-full'>{children}</div>
     </div>
 );
