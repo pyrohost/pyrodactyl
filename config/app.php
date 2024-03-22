6c37fba6825d34826c026541d24f722b19cb5406
fero <hi@evan.graphics>
2024-03-21 19:50:44 -0700

    1.0.1-rc1 Build 101100

--- config/app.php
+++ config/app.php
@@ -11,7 +11,7 @@ return [
     | change this value if you are not maintaining your own internal versions.
     */
 
-    'version' => '1.0.0-rc1',
+    'version' => '1.0.1-rc1',
 
     /*
     |--------------------------------------------------------------------------
