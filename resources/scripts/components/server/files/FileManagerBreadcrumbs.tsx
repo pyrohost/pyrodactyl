820a3b2697cfb3eac2e9a17bbb5cabdc6c1b258d
fero <hi@evan.graphics>
2024-03-25 15:49:53 -0700

    fix: page overflow when there are multiple breadcrumbs while editing

--- resources/scripts/components/server/files/FileManagerBreadcrumbs.tsx
+++ resources/scripts/components/server/files/FileManagerBreadcrumbs.tsx
@@ -87,7 +87,7 @@ export default ({ renderLeft, withinFileEditor, isNewFile }: Props) => {
                             viewBox='0 0 24 24'
                             strokeWidth={1.5}
                             stroke='currentColor'
-                            className='w-6 h-6'
+                            className='w-3 h-3'
                         >
                             <path strokeLinecap='round' strokeLinejoin='round' d='m8.25 4.5 7.5 7.5-7.5 7.5' />
                         </svg>
