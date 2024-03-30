a5b72e81a65e573b28bcfe45241daf0fb6241319
fero <hi@evan.graphics>
2024-03-29 17:37:46 -0700

    style: improve file name modal styles

--- resources/scripts/components/server/files/FileNameModal.tsx
+++ resources/scripts/components/server/files/FileNameModal.tsx
@@ -4,7 +4,7 @@ import { object, string } from 'yup';
 import Field from '@/components/elements/Field';
 import { ServerContext } from '@/state/server';
 import { join } from 'pathe';
-import Button from '@/components/elements/Button';
+import { Button } from '@/components/elements/button/index';
 
 type Props = RequiredModalProps & {
     onFileNamed: (name: string) => void;
@@ -38,7 +38,7 @@ export default ({ onFileNamed, onDismissed, ...props }: Props) => {
                     }}
                     {...props}
                 >
-                    <Form>
+                    <Form className='m-0 w-full flex flex-col gap-4'>
                         <Field
                             id={'fileName'}
                             name={'fileName'}
@@ -46,7 +46,7 @@ export default ({ onFileNamed, onDismissed, ...props }: Props) => {
                             description={'Enter the name that this file should be saved as.'}
                             autoFocus
                         />
-                        <div className={`mt-6 text-right`}>
+                        <div className={`flex justify-end w-full my-4`}>
                             <Button>Create File</Button>
                         </div>
                     </Form>
