a0c40f1fe1b6a85c014cb1e30dadf7686cd6c723
fero <hi@evan.graphics>
2024-04-03 22:40:03 -0700

    1.2.2 122000 (stable)

--- config/app.php
+++ config/app.php
@@ -11,7 +11,7 @@ return [
     | change this value if you are not maintaining your own internal versions.
     */
 
-    'version' => '1.2.1',
+    'version' => '1.2.2',
 
     /*
     |--------------------------------------------------------------------------
