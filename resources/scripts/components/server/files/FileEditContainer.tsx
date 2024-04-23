9fb7656ac4b3206df0e1eb92f229d81fbd3f1613
prplwtf <hello@prpl.wtf>
2024-04-23 22:31:53 +0200

    feat `FileEditContainer`: Improve file-edit UI

--- resources/scripts/components/server/files/FileEditContainer.tsx
+++ resources/scripts/components/server/files/FileEditContainer.tsx
@@ -125,7 +125,7 @@ export default () => {
             <FlashMessageRender byKey={'files:view'} />
 
             <ErrorBoundary>
-                <div className={`flex py-6 bg-[#ffffff11] rounded-md border-[1px] border-[#ffffff07]`}>
+                <div className={`flex py-6 bg-[#ffffff11] rounded-md rounded-b-none border-[1px] border-[#ffffff07] border-b-0`}>
                     <span className='-ml-[2rem]'></span>
                     <FileManagerBreadcrumbs withinFileEditor isNewFile={action !== 'edit'} />
                 </div>
@@ -153,7 +153,7 @@ export default () => {
                 }}
             />
 
-            <div className={`relative h-full [&>div>div]:h-full [&>div>div]:!outline-none w-full`}>
+            <div className={`relative h-full bg-[#ffffff11] border-[1px] border-[#ffffff07] border-t-0 [&>div>div]:h-full [&>div>div]:!outline-none w-full`}>
                 <Editor
                     style={{ height: 'calc(100vh - 86px)', width: '100%' }}
                     filename={filename}
