8b65d13193f16907f332dcfeffa513f2410f8485
fero <hi@evan.graphics>
2024-04-22 22:40:20 -0700

    1.6.0 160000 (stable)

--- config/app.php
+++ config/app.php
@@ -11,7 +11,7 @@ return [
     | change this value if you are not maintaining your own internal versions.
     */
 
-    'version' => '1.5.0',
+    'version' => '1.6.0',
 
     /*
     |--------------------------------------------------------------------------
