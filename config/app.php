0056a798ba82a9357b015a35bbcba84a56961248
fero <hi@evan.graphics>
2024-06-04 01:39:54 -0700

    chore: 2.0.0 (stable)

--- config/app.php
+++ config/app.php
@@ -11,7 +11,7 @@ return [
     | change this value if you are not maintaining your own internal versions.
     */
 
-    'version' => '1.7.0',
+    'version' => '2.0.0',
 
     /*
     |--------------------------------------------------------------------------
