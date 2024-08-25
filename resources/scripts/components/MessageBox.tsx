0639aee2519d2459a6a94df08e8edbdc1d3bebeb
he3als <65787561+he3als@users.noreply.github.com>
2024-08-26 00:36:17 +0100

    feat: update UI of MessageBox

--- resources/scripts/components/MessageBox.tsx
+++ resources/scripts/components/MessageBox.tsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import Code from './elements/Code';
+
 export type FlashMessageType = 'success' | 'info' | 'warning' | 'error';
 
 interface Props {
@@ -12,9 +14,13 @@ const Container = styled.div<{ $type?: FlashMessageType }>``;
 Container.displayName = 'MessageBox.Container';
 
 const MessageBox = ({ title, children, type }: Props) => (
-    <Container className='flex items-center gap-2 bg-[#ffffff11] px-4 py-2 rounded-full' $type={type} role={'alert'}>
-        {title && <h2 className='uppercase text-xs font-bold'>{title}</h2>}
-        <p>{children}</p>
+    <Container
+        className='flex flex-col gap-2 bg-black border-[2px] border-brand/70 p-4 rounded-2xl mb-4'
+        $type={type}
+        role={'alert'}
+    >
+        {title && <h2 className='font-bold text-xl'>{title}</h2>}
+        <Code>{children}</Code>
     </Container>
 );
 MessageBox.displayName = 'MessageBox';
