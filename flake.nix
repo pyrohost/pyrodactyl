c28467da62fdc800ee27881085a8699919b56112
KalebSchmidlkofer <kaleb@ashbyte.com>
2024-08-25 13:06:38 -0700

    added preinstall to flake.nix and more packages

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Development environment for Pyrodactyl Panel";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
 
   outputs = { self, nixpkgs }:
@@ -12,44 +12,60 @@
     in {
       packages.${system}.default = pkgs.stdenv.mkDerivation {
         pname = "Pyrodactyl-Panel";
-        version = "1.0.0";
+        version = "4.0.0-dev";
 
         src = ./.;
 
         buildInputs = [
-          pkgs.python310
-          pkgs.docker
           pkgs.cowsay
           pkgs.lolcat
-          pkgs.php
-          pkgs.php83Packages.composer
-
         ];
 
         buildPhase = ''
-          echo "Building Pyrodactyl Panel..."
-          # Add build commands here
+          set -x
+          echo "Build system doesn't work Yet!"
         '';
 
         installPhase = ''
-          echo "Installing Pyrodactyl Panel..."
-          # Add install commands here
+          mkdir -p $out
+          cp -r * $out/
+        '';
+
+        preInstall = ''
+          npm i -g pnpm
+          pnpm add -g pnpm
+          pnpm i -g turbo
+          composer install --no-dev --optimize-autoloader
+          npm run ship
         '';
       };
 
       devShells.${system} = pkgs.mkShell {
         buildInputs = [
-          pkgs.python310
-          pkgs.docker
           pkgs.cowsay
           pkgs.lolcat
           pkgs.php
           pkgs.php83Packages.composer
+          pkgs.php82Extensions.tokenizer
+          pkgs.nodejs_20
+          pkgs.mariadb_110
+          pkgs.redis
+          pkgs.nginx
+          pkgs.docker
+          pkgs.docker-compose
+          pkgs.openssl
         ];
 
         shellHook = ''
           echo "Welcome to the development environment for Pyrodactyl Panel!" | cowsay | lolcat
         '';
       };
+
+      apps.${system}.default = {
+        type = "app";
+        program = "${pkgs.nodejs_20}/bin/npm";
+        args = [ "run ship" ];
+      };
     };
 }
+
