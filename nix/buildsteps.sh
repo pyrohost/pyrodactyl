46256fbec2c5e009ac1def9cb89c8a3dc00bf906
KalebSchmidlkofer <kaleb@ashbyte.com>
2024-09-15 22:39:33 -0700

    chore: moved buildsteps to nix folder

--- nix/buildsteps.sh
+++ nix/buildsteps.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-mysqlrootpass="pyro"
+mysqlrootpass="password"
+mysqluser="pyro"
 
-#? All Mysql commands have to be run as root in the NixOS Environment
+docker-compose --project-directory ./docker/ up -d
+
+
 # Create the database for the panel
-sudo mysql -u root -p$mysqlrootpass -e "CREATE DATABASE IF NOT EXISTS panel;" \
+mysql -u $mysqluser -p$mysqlrootpass -e "CREATE DATABASE IF NOT EXISTS panel;" \
     -e "CREATE USER 'pyrodactyl'@'%' IDENTIFIED BY 'password';" \
     -e "GRANT ALL PRIVILEGES ON panel.% TO 'pyrodactyl'@'%' WITH GRANT OPTION;" \
     -e "FLUSH PRIVILEGES;"
 
 # Create the database host user
-sudo mysql -u root -p$mysqlrootpass -e "CREATE USER IF NOT EXISTS 'pyrodactyluser'@'%' IDENTIFIED BY 'pyrodactyl';" \
+mysql -u $mysqluser -p$mysqlrootpass -e "CREATE USER IF NOT EXISTS 'pyrodactyluser'@'%' IDENTIFIED BY 'pyrodactyl';" \
     -e "GRANT ALL PRIVILEGES ON *.* TO 'pyrodactyluser'@'%' WITH GRANT OPTION;" \
     -e "FLUSH PRIVILEGES;"
 
@@ -30,21 +33,21 @@ php artisan migrate --seed --force
 # Adding --admin tag just in case it ever gets fixed
 php artisan p:user:make -n --email dev@pyro.host --username dev --name-first Developer --name-last User --password password --admin
 # --admin probably won't be fixed due to security concerns
-sudo mysql -u root -p$mysqlrootpass -e "USE panel; UPDATE users SET root_admin = 1;" # workaround because --admin is broken
+mysql -u $mysqluser -p$mysqlrootpass -e "USE panel; UPDATE users SET root_admin = 1;" # workaround because --admin is broken
 
 # Make a location and node for the panel
 php artisan p:location:make -n --short local --long Local
 php artisan p:node:make -n --name local --description "Development Node" --locationId 1 --fqdn localhost --public 1 --scheme http --proxy 0 --maxMemory 1024 --maxDisk 10240 --overallocateMemory 0 --overallocateDisk 0
 
 # Add some dummy allocations to the node
-sudo mysql -u root -p$mysqlrootpass -e "USE panel; INSERT INTO allocations (node_id, ip, port) VALUES (1, 'localhost', 25565), (1, 'localhost', 25566), (1, 'localhost', 25567);"
+mysql -u $mysqluser  -p$mysqlrootpass -e "USE panel; INSERT INTO allocations (node_id, ip, port) VALUES (1, 'localhost', 25565), (1, 'localhost', 25566), (1, 'localhost', 25567);"
 
 # Setup wings
 #curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
 #chmod u+x /usr/local/bin/wings
 
 # Configure wings
-# This needs to be changed into a docker-compose file probably, or just a docker cli command
+# This needs to be changed into a docker-compose file probably for better and easier editing
 mkdir $(pwd)/nix/pterodactyl
 mkdir $(pwd)/nix/pterodactyl/etc
 mkdir $(pwd)/nix/pterodactyl/var
