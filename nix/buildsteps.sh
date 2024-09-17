da819793ef1715deaf39e5df545df30c985c4dec
KalebSchmidlkofer <kaleb@ashbyte.com>
2024-09-16 18:15:02 -0700

    updated some nixos specific "Features" and ensure mariadb and wings folders exists

--- nix/buildsteps.sh
+++ nix/buildsteps.sh
@@ -3,19 +3,24 @@
 mysqlrootpass="password"
 mysqluser="root"
 
-docker-compose --project-directory ./docker/maria up -d
+docker-compose --project-directory ./nix/docker/maria/ up -d --force-recreate
 
 
 # Create the database for the panel
-mysql -u $mysqluser -p$mysqlrootpass -h 0.0.0.0 -e "CREATE DATABASE IF NOT EXISTS panel;" \
-    -e "CREATE USER 'pyrodactyl'@'%' IDENTIFIED BY 'password';" \
-    -e "GRANT ALL PRIVILEGES ON panel.% TO 'pyrodactyl'@'%' WITH GRANT OPTION;" \
-    -e "FLUSH PRIVILEGES;"
+mysql -u "$mysqluser" -p"$mysqlrootpass" -h 127.0.0.1 -e "
+    CREATE DATABASE IF NOT EXISTS panel;
+    CREATE USER IF NOT EXISTS 'pyrodactyl'@'%' IDENTIFIED BY 'password';
+    GRANT ALL PRIVILEGES ON panel.* TO 'pyrodactyl'@'%' WITH GRANT OPTION;
+    FLUSH PRIVILEGES;
+"
+
 
 # Create the database host user
-mysql -u $mysqluser -p$mysqlrootpass -h 0.0.0.0 -e "CREATE USER IF NOT EXISTS 'pyrodactyluser'@'%' IDENTIFIED BY 'pyrodactyl';" \
-    -e "GRANT ALL PRIVILEGES ON *.* TO 'pyrodactyluser'@'%' WITH GRANT OPTION;" \
-    -e "FLUSH PRIVILEGES;"
+mysql -u "$mysqluser" -p"$mysqlrootpass" -h 127.0.0.1 -e "
+    CREATE USER IF NOT EXISTS 'pyrodactyluser'@'%' IDENTIFIED BY 'pyrodactyl';
+    GRANT ALL PRIVILEGES ON panel.* TO 'pyrodactyluser'@'%' WITH GRANT OPTION;
+    FLUSH PRIVILEGES;
+"
 
 # Initialize the Pterodactyl panel
 
@@ -25,7 +30,7 @@ php /usr/local/bin/composer install --no-dev --optimize-autoloader
 
 # PHP Artisan commands
 php artisan key:generate --force
-php artisan p:environment:setup -n --author dev@pyro.host --url http://localhost:3000 --cache redis --session redis --queue redis
+php artisan p:environment:setup -n --author dev@pyro.host --url http://localhost:8000 --cache redis --session redis --queue redis
 php artisan p:environment:database -n --host 127.0.0.1 --port 3306 --database panel --username pyrodactyluser --password pyrodactyl
 php artisan migrate --seed --force
 
@@ -33,7 +38,7 @@ php artisan migrate --seed --force
 # Adding --admin tag just in case it ever gets fixed
 php artisan p:user:make -n --email dev@pyro.host --username dev --name-first Developer --name-last User --password password --admin
 # --admin probably won't be fixed due to security concerns
-mysql -u $mysqluser -p$mysqlrootpass --h 0.0.0.0 -e "USE panel; UPDATE users SET root_admin = 1;" # workaround because --admin is broken
+mysql -u $mysqluser -p$mysqlrootpass -h 0.0.0.0 -e "USE panel; UPDATE users SET root_admin = 1;" # workaround because --admin is broken
 
 # Make a location and node for the panel
 php artisan p:location:make -n --short local --long Local
@@ -46,12 +51,31 @@ mysql -u $mysqluser -p$mysqlrootpass -h 0.0.0.0 -e "USE panel; INSERT INTO alloc
 #curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
 #chmod u+x /usr/local/bin/wings
 
+# Delete Wings folders
+rm -fR $(pwd)/nix/docker/wings/etc
+rm -fR $(pwd)/nix/docker/wings/var
+rm -fR $(pwd)/nix/docker/wings/tmp
 
+# Delete Maria Folders
+rm -fR $(pwd)/nix/docker/maria/mariadb_data
+
+# Create Maria Folders
+mkdir $(pwd)/nix/docker/maria/mariadb_data
+
+
+# create Wings folders
 mkdir $(pwd)/nix/docker/wings/etc
 mkdir $(pwd)/nix/docker/wings/var
-mkdir $(pwd)/nix/docker/tmp
+mkdir $(pwd)/nix/docker/wings/tmp
+
 
 php artisan p:node:configuration 1 >$(pwd)/nix/docker/wings/etc/config.yml
 
-docker-compose --project-directory ./docker/wings up -d
+docker-compose --project-directory ./nix/docker/wings up -d --force-recreate
+
+php artisan serve
+
+
+docker-compose --project-directory ./nix/docker/wings down
+docker-compose --project-directory ./nix/docker/maria/ down
 
