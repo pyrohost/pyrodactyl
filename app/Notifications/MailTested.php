36d3c229b715bc92caa656b57d290ad445c020db
KalebSchmidlkofer <kaleb@ashbyte.com>
2025-01-27 19:42:28 -0800

    updated mail settings because, we are "pyro"

--- app/Notifications/MailTested.php
+++ app/Notifications/MailTested.php
@@ -20,8 +20,8 @@ class MailTested extends Notification
     public function toMail(): MailMessage
     {
         return (new MailMessage())
-            ->subject('Pterodactyl Test Message')
+            ->subject('Pyrodactyl Test Message')
             ->greeting('Hello ' . $this->user->name . '!')
-            ->line('This is a test of the Pterodactyl mail system. You\'re good to go!');
+            ->line('This is a test of the Pyrodactyl mail system. You\'re good to go!');
     }
 }
