0a8e43d4870980a693876d6dc7eaf1cf24d417b7
KushiKreates <136042271+KushiKreates@users.noreply.github.com>
2025-01-30 20:33:50 +0530

    sex kids

--- app/Exceptions/Service/Server/ResourceValidatorService.php
+++ app/Exceptions/Service/Server/ResourceValidatorService.php
@@ -14,46 +14,60 @@ use Pterodactyl\Exceptions\DisplayException;
 class ResourceValidatorService
 {
     public function validate(array $data, Server $server, User $user): void 
-    {
-        $resourcesLeft = [
-            'memory' => $user->limits['memory'] - $user->resources['memory'],
-            'disk' => $user->limits['disk'] - $user->resources['disk'],
-            'cpu' => $user->limits['cpu'] - $user->resources['cpu'],
-            'allocations' => $user->limits['allocations'] - $user->resources['allocations'],
-            'backups' => $user->limits['backups'] - $user->resources['backups'],
-            'databases' => $user->limits['databases'] - $user->resources['databases'],
-        ];
+{
+    // Calculate resource differences (how much is being added/removed)
+    $resourceChanges = [
+        'memory' => $data['memory'] - $server->memory,
+        'disk' => $data['disk'] - $server->disk,
+        'cpu' => $data['cpu'] - $server->cpu,
+        'allocations' => $data['allocation_limit'] - $server->allocation_limit,
+        'databases' => $data['database_limit'] - $server->database_limit,
+        'backups' => $data['backup_limit'] - $server->backup_limit,
+    ];
 
-        // Add current server resources back
-        $resourcesLeft['memory'] += $server->memory;
-        $resourcesLeft['disk'] += $server->disk;
-        $resourcesLeft['cpu'] += $server->cpu;
-        $resourcesLeft['allocations'] += $server->allocation_limit;
-        $resourcesLeft['databases'] += $server->database_limit;
-        $resourcesLeft['backups'] += $server->backup_limit;
+    // Calculate available resources (excluding current server's usage)
+    $availableResources = [
+        'memory' => $user->limits['memory'] - $user->resources['memory'],
+        'disk' => $user->limits['disk'] - $user->resources['disk'],
+        'cpu' => $user->limits['cpu'] - $user->resources['cpu'],
+        'allocations' => $user->limits['allocations'] - $user->resources['allocations'],
+        'databases' => $user->limits['databases'] - $user->resources['databases'],
+        'backups' => $user->limits['backups'] - $user->resources['backups'],
+    ];
 
-        if ($data['memory'] <= 0 || $data['memory'] > $resourcesLeft['memory']) {
-            throw new DisplayException('Invalid memory allocation specified.');
-        }
-
-        if ($data['disk'] <= 0 || $data['disk'] > $resourcesLeft['disk']) {
-            throw new DisplayException('Invalid disk space specified.');
-        }
-
-        if ($data['cpu'] <= 0 || $data['cpu'] > $resourcesLeft['cpu']) {
-            throw new DisplayException('Invalid CPU limit specified.');
-        }
-
-        if ($data['allocation_limit'] <= 0 || $data['allocation_limit'] > $resourcesLeft['allocations']) {
-            throw new DisplayException('Invalid allocation limit specified.');
-        }
-
-        if ($data['database_limit'] < 0 || $data['database_limit'] > $resourcesLeft['databases']) {
-            throw new DisplayException('Invalid database limit specified.');
-        }
-
-        if ($data['backup_limit'] < 0 || $data['backup_limit'] > $resourcesLeft['backups']) {
-            throw new DisplayException('Invalid backup limit specified.');
-        }
+    // Validate each resource change
+    if ($data['memory'] <= 0) {
+        throw new DisplayException('Memory must be greater than 0');
     }
+    if ($resourceChanges['memory'] > $availableResources['memory']) {
+        throw new DisplayException('Insufficient memory resources. You are trying to add ' . $resourceChanges['memory'] . 'MB more than available.');
+    }
+
+    if ($data['disk'] <= 0) {
+        throw new DisplayException('Disk must be greater than 0');
+    }
+    if ($resourceChanges['disk'] > $availableResources['disk']) {
+        throw new DisplayException('Insufficient disk resources. You are trying to add ' . $resourceChanges['disk'] . 'MB more than available.');
+    }
+
+    if ($data['cpu'] <= 0) {
+        throw new DisplayException('CPU must be greater than 0');
+    }
+    if ($resourceChanges['cpu'] > $availableResources['cpu']) {
+        throw new DisplayException('Insufficient CPU resources. You are trying to add ' . $resourceChanges['cpu'] . '% more than available.');
+    }
+
+    // Optional resources can be 0
+    if ($resourceChanges['allocations'] > $availableResources['allocations']) {
+        throw new DisplayException('Insufficient allocation resources available.');
+    }
+
+    if ($resourceChanges['databases'] > $availableResources['databases']) {
+        throw new DisplayException('Insufficient database resources available.');
+    }
+
+    if ($resourceChanges['backups'] > $availableResources['backups']) {
+        throw new DisplayException('Insufficient backup resources available.');
+    }
+}
 }
