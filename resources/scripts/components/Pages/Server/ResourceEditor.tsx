1824dc03743ffc9ff7c66804de5e68c277419999
KushiKreates <136042271+KushiKreates@users.noreply.github.com>
2025-02-02 03:20:59 +0530

    ojbji

--- resources/scripts/components/Pages/Server/ResourceEditor.tsx
+++ resources/scripts/components/Pages/Server/ResourceEditor.tsx
@@ -13,7 +13,7 @@ import {
   AlertDialogCancel,
 } from "@/components/ui/alert-dialog"
 import { LucideAlertCircle } from "lucide-react"
-import { useToast } from "@/components/ui/use-toast"
+import { useToast } from "@/hooks/use-toast";
 
 interface ServerPageProps {
   server: {
