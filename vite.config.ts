6795697eb8dd3818587353b22ca271faac0250c7
KushiKreates <136042271+KushiKreates@users.noreply.github.com>
2025-02-14 17:19:04 +0530

    fixing vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,23 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from "@vitejs/plugin-react-swc";
 import path from 'path';
+import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
-    //server: {
-       // host: "::1",
-     //   port: 5173
-    // }, 
+    server: {
+        host: "::1", // Bind to IPv6
+        port: 5173,
+        strictPort: true,
+    }, 
     // comment the above server when working on local development 
     plugins: [
         laravel({
             input: 'resources/scripts/components/App.tsx',
             refresh: true,
+            https: true
         }),
         react(),
+        mkcert() 
     ],
     resolve: {
         alias: {
