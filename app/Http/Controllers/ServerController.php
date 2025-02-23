944abc5cefb314476b97ccc83b56b96754f00f50
KushiKreates <136042271+KushiKreates@users.noreply.github.com>
2025-02-23 08:25:31 +0530

    update controllers

--- app/Http/Controllers/ServerController.php
+++ app/Http/Controllers/ServerController.php
@@ -474,4 +474,58 @@ public function __construct(
             
         }
     }
+
+    public function show(Request $request, $uuidShort)
+    {
+        try {
+            $server = Server::where('uuidShort', $uuidShort)->firstOrFail();
+            // Check the server's plan
+            $this->checkServerPlan($request, $server);
+
+            $isSuspendedDueToPlan = $this->checkServerPlan($request, $server);
+
+            \Log::info('Server suspension check', [
+                'server_id' => $server->id,
+                'is_suspended' => $server->is_suspended,
+                'status' => $server->status
+            ]);
+
+            // Check if server is suspended due to expired plan
+            if ($isSuspendedDueToPlan) {
+                return Inertia::render('Errors/Server/Expired');
+            }
+
+            
+
+            \Log::info('Server suspension check', [
+                'server_id' => $server->id,
+                'is_suspended' => $server->is_suspended,
+                'status' => $server->status
+            ]);
+            
+            // Check if server is suspended
+            if ($server->status === 'suspended') {
+                return Inertia::render('Errors/Server/Suspended');
+            }
+
+            if ($server->status === 'installing') {
+                return Inertia::render('Errors/Server/Install');
+            }
+            
+            $transformer = new ServerTransformer();
+            $transformer->setRequest($request);
+            
+            $resource = new Item($server, $transformer);                              
+            $transformed = $this->fractal->createData($resource)->toArray();
+            
+            return Inertia::render('Server/', [
+                'server' => array_merge($transformed['data'], [
+                    'is_server_owner' => $request->user()->id === $server->owner_id,
+                    'user_permissions' => $this->permissionsService->handle($server, $request->user()),
+                ])
+            ]);
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            return Inertia::render('Errors/Server/404');
+        }
+    }
 }
--- routes/base.php
+++ routes/base.php
@@ -85,6 +85,7 @@ Route::prefix('/')->group(function () {
         Route::get('/server/{uuidShort}/files', [ServerController::class, 'files'])->name('server.files');
         Route::get('/server/{uuidShort}/settings', [ServerController::class, 'settings'])->name('server.settings');
         Route::get('/server/{uuidShort}/etc', [ServerController::class, 'etc'])->name('server.etc');
+        Route::get('/server/{uuidShort}/plugins', [ServerController::class, 'plugins'])->name('server.plugins');
         Route::get('/server/{uuidShort}/upgrade', [ServerController::class, 'upgrade'])->name('server.upgrade');
         
     });
