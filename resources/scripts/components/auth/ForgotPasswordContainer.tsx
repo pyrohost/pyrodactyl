9ee90d8283ac8d7c7eaacefeaa7a4387407fc006
KalebSchmidlkofer <kaleb@ashbyte.com>
2025-02-23 03:09:40 -0800

    feat: fix #177, fix #178

--- resources/scripts/components/auth/ForgotPasswordContainer.tsx
+++ resources/scripts/components/auth/ForgotPasswordContainer.tsx
@@ -2,11 +2,13 @@ import { useStoreState } from 'easy-peasy';
 import type { FormikHelpers } from 'formik';
 import { Formik } from 'formik';
 import { useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Reaptcha from 'reaptcha';
 import { object, string } from 'yup';
 
 import LoginFormContainer from '@/components/auth/LoginFormContainer';
 import Button from '@/components/elements/Button';
+import ContentBox from '@/components/elements/ContentBox';
 import Field from '@/components/elements/Field';
 
 import requestPasswordResetEmail from '@/api/auth/requestPasswordResetEmail';
@@ -67,68 +69,72 @@ export default () => {
     };
 
     return (
-        <Formik
-            onSubmit={handleSubmission}
-            initialValues={{ email: '' }}
-            validationSchema={object().shape({
-                email: string()
-                    .email('Please enter your email address to reset your password.')
-                    .required('Please enter your email address to reset your password.'),
-            })}
-        >
-            {({ isSubmitting, setSubmitting, submitForm }) => (
-                <LoginFormContainer className={`w-full flex`}>
-                    <div className='flex h-12 mb-4 items-center w-full'>
-                        {/* temp src */}
-                        {/* <img
-                            className='w-full max-w-full h-full'
-                            loading='lazy'
-                            decoding='async'
-                            alt=''
-                            aria-hidden
-                            style={{
-                                color: 'transparent',
-                            }}
-                            src='https://i.imgur.com/Hbum4fc.png'
-                        /> */}
-                        {/* <NavLink to={'/'} className='flex shrink-0 h-full w-fit'> */}
-                        <Logo />
-                    </div>
-                    <div aria-hidden className='my-8 bg-[#ffffff33] min-h-[1px]'></div>
-                    <h2 className='text-xl font-extrabold mb-2'>Reset Password</h2>
-                    <div className='text-sm mb-6'>We&apos;ll send you an email with a link to reset your password.</div>
-                    <Field id='email' label={'Email'} name={'email'} type={'email'} />
-                    <div className={`mt-6`}>
-                        <Button
-                            className={`w-full mt-4 rounded-full bg-brand border-0 ring-0 outline-none capitalize font-bold text-sm py-2`}
-                            type={'submit'}
-                            size={'xlarge'}
-                            isLoading={isSubmitting}
-                            disabled={isSubmitting}
+        <ContentBox>
+            <Formik
+                onSubmit={handleSubmission}
+                initialValues={{ email: '' }}
+                validationSchema={object().shape({
+                    email: string()
+                        .email('Please enter your email address to reset your password.')
+                        .required('Please enter your email address to reset your password.'),
+                })}
+            >
+                {({ isSubmitting, setSubmitting, submitForm }) => (
+                    <LoginFormContainer className={`w-full flex`}>
+                        <Link to='/'>
+                            <div className='flex h-12 mb-4 items-center w-full'>
+                                <Logo />
+                            </div>
+                        </Link>
+                        <div aria-hidden className='my-8 bg-[#ffffff33] min-h-[1px]'></div>
+                        <h2 className='text-xl font-extrabold mb-2'>Reset Password</h2>
+                        <div className='text-sm mb-6'>
+                            We&apos;ll send you an email with a link to reset your password.
+                        </div>
+                        <Field id='email' label={'Email'} name={'email'} type={'email'} />
+                        <div className={`mt-6`}>
+                            <Button
+                                className={`w-full mt-4 rounded-full bg-brand border-0 ring-0 outline-none capitalize font-bold text-sm py-2`}
+                                type={'submit'}
+                                size={'xlarge'}
+                                isLoading={isSubmitting}
+                                disabled={isSubmitting}
+                            >
+                                Send Email
+                            </Button>
+                        </div>
+                        {recaptchaEnabled && (
+                            <Reaptcha
+                                ref={ref}
+                                size={'invisible'}
+                                sitekey={siteKey || '_invalid_key'}
+                                onVerify={(response) => {
+                                    setToken(response);
+                                    setTimeout(() => {
+                                        submitForm();
+                                    }, 0);
+                                }}
+                                onExpire={() => {
+                                    setSubmitting(false);
+                                    setToken('');
+                                }}
+                            />
+                        )}
+                        <div aria-hidden className='my-8 bg-[#ffffff33] min-h-[1px]'></div>
+
+                        <div
+                            className={`text-center w-full rounded-lg bg-[#ffffff33] border-0 ring-0 outline-none capitalize font-bold text-sm py-2 `}
                         >
-                            Send Email
-                        </Button>
-                    </div>
-                    {recaptchaEnabled && (
-                        <Reaptcha
-                            ref={ref}
-                            size={'invisible'}
-                            sitekey={siteKey || '_invalid_key'}
-                            onVerify={(response) => {
-                                setToken(response);
-                                // Ensure submitForm is called *after* token is updated
-                                setTimeout(() => {
-                                    submitForm();
-                                }, 0);
-                            }}
-                            onExpire={() => {
-                                setSubmitting(false);
-                                setToken('');
-                            }}
-                        />
-                    )}
-                </LoginFormContainer>
-            )}
-        </Formik>
+                            <Link
+                                to={'/auth/login'}
+                                className={`text-xs text-white tracking-wide uppercase no-underline hover:text-neutral-700 border-color-[#ffffff33] pt-4`}
+                            >
+                                Return to Login
+                            </Link>
+                        </div>
+                    </LoginFormContainer>
+                )}
+            </Formik>
+        </ContentBox>
     );
 };
