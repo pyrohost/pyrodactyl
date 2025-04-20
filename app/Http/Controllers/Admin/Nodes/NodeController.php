c14c80950613413a12dff365b2e2609af80a93d7
naterfute <naterfute@pyro.host>
2025-04-20 13:46:46 -0700

    lint: formatting

--- app/Http/Controllers/Admin/Nodes/NodeController.php
+++ app/Http/Controllers/Admin/Nodes/NodeController.php
@@ -11,25 +11,25 @@ use Illuminate\Contracts\View\Factory as ViewFactory;
 
 class NodeController extends Controller
 {
-    /**
-     * NodeController constructor.
-     */
-    public function __construct(private ViewFactory $view)
-    {
-    }
+  /**
+   * NodeController constructor.
+   */
+  public function __construct(private ViewFactory $view)
+  {
+  }
 
-    /**
-     * Returns a listing of nodes on the system.
-     */
-    public function index(Request $request): View
-    {
-        $nodes = QueryBuilder::for(
-            Node::query()->with('location')->withCount('servers')
-        )
-            ->allowedFilters(['uuid', 'name'])
-            ->allowedSorts(['id'])
-            ->paginate(25);
+  /**
+   * Returns a listing of nodes on the system.
+   */
+  public function index(Request $request): View
+  {
+    $nodes = QueryBuilder::for(
+      Node::query()->with('location')->withCount('servers')
+    )
+      ->allowedFilters(['uuid', 'name'])
+      ->allowedSorts(['id'])
+      ->paginate(25);
 
-        return $this->view->make('admin.nodes.index', ['nodes' => $nodes]);
-    }
+    return $this->view->make('admin.nodes.index', ['nodes' => $nodes]);
+  }
 }
--- app/Http/Controllers/Admin/Nodes/SystemInformationController.php
+++ app/Http/Controllers/Admin/Nodes/SystemInformationController.php
@@ -11,30 +11,30 @@ use Pterodactyl\Repositories\Wings\DaemonConfigurationRepository;
 
 class SystemInformationController extends Controller
 {
-    /**
-     * SystemInformationController constructor.
-     */
-    public function __construct(private DaemonConfigurationRepository $repository)
-    {
-    }
+  /**
+   * SystemInformationController constructor.
+   */
+  public function __construct(private DaemonConfigurationRepository $repository)
+  {
+  }
 
-    /**
-     * Returns system information from the Daemon.
-     *
-     * @throws \Pterodactyl\Exceptions\Http\Connection\DaemonConnectionException
-     */
-    public function __invoke(Request $request, Node $node): JsonResponse
-    {
-        $data = $this->repository->setNode($node)->getSystemInformation();
+  /**
+   * Returns system information from the Daemon.
+   *
+   * @throws \Pterodactyl\Exceptions\Http\Connection\DaemonConnectionException
+   */
+  public function __invoke(Request $request, Node $node): JsonResponse
+  {
+    $data = $this->repository->setNode($node)->getSystemInformation();
 
-        return new JsonResponse([
-            'version' => $data['version'] ?? '',
-            'system' => [
-                'type' => Str::title($data['os'] ?? 'Unknown'),
-                'arch' => $data['architecture'] ?? '--',
-                'release' => $data['kernel_version'] ?? '--',
-                'cpus' => $data['cpu_count'] ?? 0,
-            ],
-        ]);
-    }
+    return new JsonResponse([
+      'version' => $data['version'] ?? '',
+      'system' => [
+        'type' => Str::title($data['os'] ?? 'Unknown'),
+        'arch' => $data['architecture'] ?? '--',
+        'release' => $data['kernel_version'] ?? '--',
+        'cpus' => $data['cpu_count'] ?? 0,
+      ],
+    ]);
+  }
 }
