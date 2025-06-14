5fc00ea31491c6d0fde346109510039d70a1a794
Lovinoes <lovinoes@lovinoes.de>
2025-06-14 18:07:30 +0200

    fix(favicon): replace corrupt favicon with updated cross-platform set
    - Replaced corrupt favicon (favicon-180x180.png) with a fresh set for better cross-browser support.
    
    - Updated <head> with new favicon tags.
    
    - Added <meta name="darkreader-lock"> to prevent Dark Reader from altering styles.

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,14 +8,15 @@
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <meta name="_token" content="{{ csrf_token() }}">
 
-  <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
-  <link rel="icon" type="image/png" href="/favicons/favicon-32x32.png" sizes="32x32">
-  <link rel="icon" type="image/png" href="/favicons/favicon-16x16.png" sizes="16x16">
-  <link rel="manifest" href="/favicons/manifest.json">
-  <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#bc6e3c">
-  <link rel="shortcut icon" href="/favicons/favicon.ico">
-  <meta name="msapplication-config" content="/favicons/browserconfig.xml">
+  <link rel="icon" type="image/png" href="/favicons/favicon-96x96.png" sizes="96x96" />
+  <link rel="icon" type="image/svg+xml" href="/favicons/favicon.svg" />
+  <link rel="shortcut icon" href="/favicons/favicon.ico" />
+  <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+  <meta name="apple-mobile-web-app-title" content="Pyrodactyl" />
+  <link rel="manifest" href="/favicons/site.webmanifest" />
+
   <meta name="theme-color" content="#000000">
+  <meta name="darkreader-lock">
 
   @include('layouts.scripts')
 
