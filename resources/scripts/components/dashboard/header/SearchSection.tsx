f0656a77f54881fa11b32b18795fc135ee8fba7f
Naterfute <naterfute@pyro.host>
2026-02-07 05:46:46 -0800

    ui: make search bar look better with animations

--- resources/scripts/components/dashboard/header/SearchSection.tsx
+++ resources/scripts/components/dashboard/header/SearchSection.tsx
@@ -1,6 +1,6 @@
 import { Search01Icon } from '@hugeicons/core-free-icons';
 import { HugeiconsIcon } from '@hugeicons/react';
-import { memo, useState } from 'react';
+import { memo, useState, useEffect, useRef } from 'react';
 
 import { Input } from '@/components/ui/input';
 import { KeyboardShortcut } from '@/components/ui/keyboard-shortcut';
@@ -22,22 +22,40 @@ interface SearchSectionProps {
 const SearchSection = memo(({ className }: SearchSectionProps) => {
     const [searchValue, setSearchValue] = useState('');
 
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        const handleKeyDown = (e: KeyboardEvent) => {
+            if ((e.ctrlKey || e.metaKey) && e.key === 'k') {
+                e.preventDefault();
+                inputRef.current?.focus();
+            }
+        };
+
+        window.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, []);
+
     return (
-        <div className={`flex items-center gap-2 h-full ${className || ''}`}>
-            <div className='relative w-full'>
+        <div className={`flex items-center gap-2 h-full group ${className || ''}`}>
+            <div className='relative w-3/4 transition-all duration-200 ease-out group group-focus-within:w-full mx-auto'>
                 <Input
                     id='header-search'
+                    ref={inputRef}
                     type='text'
                     placeholder='Search servers...'
                     value={searchValue}
                     onChange={(e) => {
                         setSearchValue(e.target.value);
                     }}
-                    className='pl-10 pr-16'
+                    className='pl-10 pr-16 mx-auto w-full group-focus-within:w-full transition-all duration-200 ease-out '
                 />
                 <SearchIcon />
                 {!searchValue && (
-                    <div className='absolute top-1/2 right-4 -translate-y-1/2 transform flex align-middle pointer-events-none'>
+                    <div className='absolute top-1/2 right-4 -translate-y-1/2 transform flex align-middle pointer-events-none group-focus-within:opacity-0 transition-all duration-200 ease-out '>
                         <KeyboardShortcut keys={['cmd', 'k']} variant='faded' />
                     </div>
                 )}
