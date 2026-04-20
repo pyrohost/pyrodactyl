d735f26708e23ae409c63310cf9d9c9eab71fff1
Naterfute <naterfute@pyro.host>
2026-04-20 14:14:00 -0700

    fixes: #55

---
+++ database/migrations/2026_04_20_211251_clear_unused_allocation_notes.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::table('allocations')
+            ->whereNull('server_id')
+            ->update(['notes' => null]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
